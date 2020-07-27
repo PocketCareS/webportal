a5a6ebcfc15ca156b4a4df924b53e32db7766346
Sourav Samanta <sourav0457@gmail.com>
2020-07-27 14:43:26 -0400

    fixing login page

--- src/login.js
+++ src/login.js
@@ -6,6 +6,7 @@ import Main from "./main";
 import App from "./App";
 import { baseUrl } from "./constants";
 import "./css/login.css";
+import { Link } from "react-router-dom";
 import AnalyticsPage from "./components/analyticsPage";
 import NYAnalytics from "./components/nyAnalyticsPage";
 import ContactTracing from "./components/contactTracing";
@@ -122,70 +123,84 @@ class LoginPage extends React.Component {
     console.log(this.props);
     return (
       // <div className="container login-container w-75">
-      <div className="outer-card-container">
-        <h1>{cookies.get("university")}</h1>
+      <React.Fragment>
+        <div className="outer-card-container">
 
-        <div className="card-container card text-center">
-          {/* <div className="col-md-6 login-form"> */}
-          <div className="card-body">
-            <h3>Welcome to PocketCare</h3>
-            <form>
-              <div className="form-group">
-                <input
-                  className="form-control"
-                  type="text"
-                  name="email"
-                  placeholder="Email"
-                  value={this.state.email}
-                  onChange={this.handleChange}
-                  onKeyPress={this.enterPressed.bind(this)}
-                  required
-                />
-                <span style={{ color: "red" }}>
-                  {this.state.fieldErrors["email"]}
-                </span>
-              </div>
-              <div className="form-group">
-                <input
-                  type="password"
-                  className="form-control"
-                  name="password"
-                  placeholder="Password"
-                  value={this.state.password}
-                  onChange={this.handleChange}
-                  onKeyPress={this.enterPressed.bind(this)}
-                  required
-                />
-                <span style={{ color: "red" }}>
-                  {this.state.fieldErrors["password"]}
-                </span>
-              </div>
-              <div className="form-group">
-                <input
-                  type="button"
-                  className="btnSubmit"
-                  value="Login"
-                  onClick={this.validateFields}
-                />
-                <br></br>
-                <span style={{ color: "red" }}>
-                  {this.state.fieldErrors["loginError"]}
-                </span>
-              </div>
-              {/* <div className="form-group">
+          <div className="card-container ">
+            <p className="login-title">Login - <span className="university-title">{cookies.get("university")}</span></p>
+            <div className="card text-center">
+              {/* <div className="col-md-6 login-form"> */}
+              <div className="card-body">
+                <div className="menu-logo-container">
+                  <img
+                    className="logo-welcome"
+                    src={require("./images/logo.png")}
+                    alt=""
+                    loading="lazy"
+                  />
+                </div>
+                <p className="login-pocketcare">PocketCare S</p>
+                <form>
+                  <div className="form-group">
+                    <input
+                      className="form-control"
+                      type="text"
+                      name="email"
+                      placeholder="Email"
+                      value={this.state.email}
+                      onChange={this.handleChange}
+                      onKeyPress={this.enterPressed.bind(this)}
+                      required
+                    />
+                    <span style={{ color: "red" }}>
+                      {this.state.fieldErrors["email"]}
+                    </span>
+                  </div>
+                  <div className="form-group">
+                    <input
+                      type="password"
+                      className="form-control"
+                      name="password"
+                      placeholder="Password"
+                      value={this.state.password}
+                      onChange={this.handleChange}
+                      onKeyPress={this.enterPressed.bind(this)}
+                      required
+                    />
+                    <span style={{ color: "red" }}>
+                      {this.state.fieldErrors["password"]}
+                    </span>
+                  </div>
+                  <div className="form-group">
+                    <input
+                      type="button"
+                      className="btn btn-primary btnSubmit"
+                      value="Login"
+                      onClick={this.validateFields}
+                    />
+                    <br></br>
+                    <span style={{ color: "red" }}>
+                      {this.state.fieldErrors["loginError"]}
+                    </span>
+                  </div>
+                  {/* <div className="form-group">
                 <a href="/forgotPassword" className="ForgotPwd">
                   Forgot Password?
                 </a>
               </div> */}
-            </form>
+                </form>
+              </div>
+            </div>
+            <Link to="/" className="change-university"> Change University? </Link>
           </div>
-        </div>
-        {/* <Switch>
+          {/* <Switch>
           <Route path="/analytics" component={AnalyticsPage} />
           <Route path="/nyanalytics" component={NYAnalytics} />
           <Route path="/tracing" component={ContactTracing} />
         </Switch> */}
-      </div>
+
+        </div>
+      </React.Fragment>
     );
   }
 }
